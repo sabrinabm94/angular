ea164f1e7c6f0be469fcee2e4905b52e709d0f8c
Sabrina <sabrinabm94@gmail.com>
2023-07-28 15:11:21 -0300

    project rename

--- divulga-servicos/src/app/app.component.spec.ts
+++ divulga-servicos/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'divulga-servicos'`, () => {
+  it(`should have as title 'busca-profissionais'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('divulga-servicos');
+    expect(app.title).toEqual('busca-profissionais');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('divulga-servicos app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('busca-profissionais app is running!');
   });
 });
